a393390097b034a20bfe8289e5c2f862637b4748
lackroy511 <lackroy5111@gmail.com>
2024-06-02 10:15:46 +0000

    admin panel

--- app/movies/admin.py
+++ app/movies/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 
-from app.movies.models import FilmWork
+from movies.models import FilmWork, Genre, Person
 
 
-# Register your models here.
 @admin.register(FilmWork)
 class FilmWorkAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'creation_date', 'age_rating', 'film_type', 'rating')
+    search_fields = ('title', 'creation_date', 'film_type', 'rating')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date')
+    search_fields = ('name', 'birth_date')
--- app/movies/models.py
+++ app/movies/models.py
@@ -13,16 +13,22 @@ class Genre(TimeStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
 
 
 class Person(TimeStampedModel):
     class Meta:
-        verbose_name = _('персона')
-        verbose_name_plural = _('персоны')
+        verbose_name = _('участник')
+        verbose_name_plural = _('участники')
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
     birth_date = models.DateField(_('Дата рождения'), auto_now=False, blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
     
 
 class FilmWork(TimeStampedModel):
@@ -36,14 +42,17 @@ class FilmWork(TimeStampedModel):
         SERIES = 'series', _('сериал')
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.TextField(_('Название'))
+    title = models.CharField(_('Название'), max_length=255)
     description = models.TextField(_('Описание'), null=True, blank=True)
     creation_date = models.DateField(_('Дата создания'), auto_now=False, auto_now_add=False)
     age_rating = models.CharField(_('Возрастной рейтинг'), max_length=10, null=True, blank=True)
     film_type = models.TextField(_('Тип'), choices=FilmType.choices, default=FilmType.MOVIE)
     file_path = models.TextField(_('Путь к файлу'), null=True, blank=True)
+    rating = models.FloatField(_('Рейтинг'), null=True, blank=True, validators=[MinValueValidator(0.0)])
     genre = models.ManyToManyField(Genre, verbose_name=_('жанр'), related_name='genre')
     director = models.ManyToManyField(Person, verbose_name=_('режиссер'), related_name='film_director')
     actor = models.ManyToManyField(Person, verbose_name=_('актер'), related_name='film_actor')
     writer = models.ManyToManyField(Person, verbose_name=_('сценарист'), related_name='film_writer')
-    test_field = models.TextField(_('qwe'), null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
